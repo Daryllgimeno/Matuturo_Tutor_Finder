e60922f3b4e37ed734a37151be628d91a6309eec
Daryllgimeno <gimenodaryll@gmail.com>
2024-12-10 14:45:38 +0800

    Update password_hash.php

--- password_hash.php
+++ password_hash.php
@@ -5,6 +5,13 @@ $password = "admin";
 // Hash the password using BCRYPT
 $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
+//how to insert hashed credentials
+// INSERT INTO users (username, password, role, email) 
+//VALUES ('admin', 'hashed_password_here', 'admin', 'admin@example.com');
+
+
 // Output the hashed password
 echo "Hashed Password: " . $hashed_password;
 ?>
+
+
